53519e16fdb34d0aa64c2224bea46fdd8fee8ac0
NikiHal013 <nikola.haluzova@unob.cz>
2025-10-16 18:36:03 +0000

    hello world

--- Ukol_0/cpp/test.cpp
+++ Ukol_0/cpp/test.cpp
@@ -1,6 +1,5 @@
-
 #include "gtest/gtest.h"
-#include "hello_world.cpp" // Předpokládám, že kód je v souboru hello_world.cpp
+#include "hello_world.cpp" // Předpokládám, že kód je v souboru vypocty.cpp
 
 TEST(TestyFunkci, hello_world)
 {
