ae4abc4f804b17e134d94809e3c3de78e410c85f
NikiHal013 <nikola.haluzova@unob.cz>
2025-10-16 18:33:34 +0000

    commit

--- Ukol_0/cpp/test.cpp
+++ Ukol_0/cpp/test.cpp
@@ -1,6 +1,6 @@
 
 #include "gtest/gtest.h"
-#include "hello_world.cpp" // Předpokládám, že kód je v souboru vypocty.cpp
+#include "hello_world.cpp" // Předpokládám, že kód je v souboru hello_world.cpp
 
 TEST(TestyFunkci, hello_world)
 {
