ef7879adbbb294dd5472028eede9890442c1bf02
Lukas Kalensky <lukas.kalensky@unob.cz>
2025-10-21 12:04:40 +0000

    21.10.2025_prvni_hodina

---
+++ Ukoly_hodin/Ukol3/main2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//std::retezec.find(cisla[i], text)
+int main() {
+    std::cout << "hello_world" << std::endl;
+}
