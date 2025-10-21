a00a115d1942a991b145cccb41e5903a559f62a7
velkayolanda <tomo.simurda@gmail.com>
2025-10-21 12:48:29 +0000

    advent of code part 2

--- Ukoly_z_hodin/Ukoly_21-10-2025/advent_of_code_2023_trebuchet.cpp
+++ Ukoly_z_hodin/Ukoly_21-10-2025/advent_of_code_2023_trebuchet.cpp
@@ -2,50 +2,72 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-  
     ifstream in;
-    
-    if (argc > 1) {
+
+    if (argc > 1)
+    {
         in.open(argv[1]);
-        if (!in) {
+        if (!in)
+        {
             cerr << "Failed to open file: " << argv[1] << '\n';
             return 2;
         }
-    } else {
-        
+    }
+    else
+    {
         in.open("input.txt");
-        if (!in) {
+        if (!in)
+        {
             in.clear();
             in.open("/dev/stdin");
-            if (!in) {
+            if (!in)
+            {
                 cerr << "Failed to open any input" << '\n';
                 return 2;
             }
         }
     }
 
+    vector<string> slova = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     string line;
-    int total = 0;
-    while (getline(in, line)) {
-        char first = '\0', last = '\0';
-        for (char c : line){
-            if (isdigit(c)) {
-                if (!first) first = c;
-                last = c;
+    long long total = 0;
+
+    while (getline(in, line))
+    {
+        vector<int> digits;  
+
+        for (size_t i = 0; i < line.size(); ++i)
+        {
+            if (isdigit(line[i]))
+            {
+                digits.push_back(line[i] - '0');
+            }
+            for (int d = 0; d < 10; ++d)
+            {
+                const string &word = slova[d];
+                if (i + word.size() <= line.size() && line.substr(i, word.size()) == word)
+                {
+                    digits.push_back(d);
+                    break;
+                }
             }
         }
-        if (first && last) {
-            int value = (first - '0') *10 + (last - '0');
+
+        if (!digits.empty())
+        {
+            int value = digits.front() * 10 + digits.back();
             total += value;
         }
-        
     }
+
     cout << "Total: " << total << '\n';
     return 0;
 }
