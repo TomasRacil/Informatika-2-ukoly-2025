b1635f9cd9cd0fbac5c44f689747bf96ed11ebda
Jan Vavřina <honzik.vavrina@gmail.com>
2025-10-28 11:22:25 +0100

    fix syntax - 2

--- ukoly_z_hodin/241025/students.cpp
+++ ukoly_z_hodin/241025/students.cpp
@@ -2,9 +2,12 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <tuple>
 
 using namespace std;
+
+// ---------------------------
+// Structs
+// ---------------------------
 
 struct Student
 {
@@ -15,39 +18,88 @@ struct Student
 
 struct Uzel
 {
-    struct Student;
-    struct Uzel *next;
+    Student data;
+    Uzel* next;
 };
 
-void parsujRadek(string& radek)
-{
-    Student new_student;
-    string radek;
-    
+// ---------------------------
+// Functions
+// ---------------------------
 
-    // return {"student"};
-}
-
-stringstream open_file(string file_name)
+// Allocates and reads entire file into stringstream
+stringstream open_file(const string& file_name)
 {
     ifstream soubor(file_name);
-
-    if (!soubor || !soubor.is_open())
+    if (!soubor.is_open())
     {
+        cerr << "File " << file_name << " could not be opened!" << endl;
         return stringstream();
     }
 
     stringstream content;
-
     content << soubor.rdbuf();
-    soubor.close();
-
     return content;
 }
+
+// Parses a line "id jmeno prumer" into a Student
+Student parsujRadek(const string& radek)
+{
+    Student new_student;
+    stringstream ss(radek);
+    ss >> new_student.id >> new_student.jmeno >> new_student.prumer;
+    return new_student;
+}
+
+// Prints all students in linked list
+void printStudents(Uzel* head)
+{
+    Uzel* current = head;
+    while (current)
+    {
+        cout << current->data.id << " "
+             << current->data.jmeno << " "
+             << current->data.prumer << endl;
+        current = current->next;
+    }
+}
+
+// Frees linked list memory
+void freeList(Uzel* head)
+{
+    while (head)
+    {
+        Uzel* tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+}
+
+// ---------------------------
+// Main
+// ---------------------------
 
 int main()
 {
     stringstream soubor_content_ss = open_file("studenti.txt");
+    string line;
+
+    Uzel* head = nullptr; // start of linked list
+
+    while (getline(soubor_content_ss, line))
+    {
+        if (line.empty()) continue; // skip empty lines
+        Student s = parsujRadek(line);
+
+        // Add to linked list (insert at head)
+        Uzel* node = new Uzel{ s, head };
+        head = node;
+    }
+
+    cout << "All students:" << endl;
+    printStudents(head);
+
+    // Free memory
+    freeList(head);
 
     return 0;
 }
