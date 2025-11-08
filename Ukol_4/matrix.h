b93aee171f6a2947f10fd6b5fee021dbda449b35
BenVosyka <ben.vosyka@gmail.com>
2025-11-08 17:04:25 +0000

    ukol4

--- Ukol_4/matrix.h
+++ Ukol_4/matrix.h
@@ -3,64 +3,37 @@
 #include <stdexcept> // Pro std::out_of_range
 #include <ostream>   // Pro std::ostream
 
-/**
- * @brief Třída reprezentující matici a operace nad ní.
- * * TODO: Zkopírujte sem definici (deklaraci) vaší třídy Matrix z Ukolu 3.
- * Níže jsou uvedeny metody, které by třída měla obsahovat,
- * plus nové operátory pro Ukol 4.
- */
-class Matrix {
+class Matrix
+{
 private:
     int rows_;
     int cols_;
-    int** data_;
+    int **data_;
 
-    // TODO: Zkopírujte sem deklarace soukromých metod (allocateMatrix, deallocateMatrix)
     void allocateMatrix();
     void deallocateMatrix();
 
 public:
-    // TODO: Zkopírujte sem deklarace veřejných metod z Ukolu 3
-    // (Konstruktor, Destruktor, Kopírovací konstruktor, gettery, settery,
-    // subtract, transpose)
-
-    // Příklad (doplňte zbytek):
-    Matrix();
+    // Konstruktory a destruktor
+    Matrix(); // default konstruktor pro 0x0 matici
     Matrix(int rows, int cols);
+    Matrix(const Matrix &other); // kopírovací konstruktor
     ~Matrix();
-    Matrix(const Matrix& other);
-    
+
+    // Gettery a settery
     int getRows() const;
     int getCols() const;
     int getValue(int row, int col) const;
     void setValue(int row, int col, int value);
-    
-    Matrix subtract(const Matrix& other);
-    Matrix T();
 
-    // Deklarace pro operátory +, *, <<
+    // Metody
+    Matrix subtract(const Matrix &other) const;
+    Matrix T() const; // transpozice (dříve transpose)
 
-    /**
-     * @brief Přetížení operátoru + pro sčítání matic.
-     * Nahrazuje metodu 'add'.
-     */
-    Matrix operator+(const Matrix& other);
-
-    /**
-     * @brief Přetížení operátoru * pro násobení matic.
-     * Nahrazuje metodu 'multiply'.
-     */
-    Matrix operator*(const Matrix& other);
-
-    /**
-     * @brief Přetížení operátoru * pro násobení skalárem  (Matrix*scalar).
-     */
-    Matrix operator*(int scalar);
-
-    /**
-     * @brief Přetížení operátoru << pro výpis matice do std::ostream.
-     */
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& mat);
-
-
+    // Operátory
+    Matrix operator+(const Matrix &other) const;
+    Matrix operator*(const Matrix &other) const;
+    Matrix operator*(int scalar) const;
+    Matrix &operator=(const Matrix &other);
+    friend std::ostream &operator<<(std::ostream &os, const Matrix &mat);
 };
