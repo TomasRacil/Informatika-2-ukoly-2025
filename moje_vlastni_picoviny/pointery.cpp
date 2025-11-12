214d05e33a38d12fd4ba5608dc3a71159913ebd9
Svejkys <x5pm2q29r5@privaterelay.appleid.com>
2025-11-12 15:49:49 +0000

    ukoly z hodin

--- moje_vlastni_picoviny/pointery.cpp
+++ moje_vlastni_picoviny/pointery.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-using namespace std;
 
-int main(){
-    int radky, sloupce;
-    cout << "Zadej pocet radku a sloupcu: " << endl;
-    cin >> radky >> sloupce;
+int main() {
+    int a = 10;
+    int* p = &a; // pointer na proměnnou a
 
-    int** matice = new int*[radky];
-    for(int i = 0; i < radky; i++){
-        matice[i] = new int[sloupce];
-    }
+    std::cout << "Hodnota a: " << a << std::endl;
+    std::cout << "Adresa a: " << &a << std::endl;
+    std::cout << "Hodnota přes pointer p: " << *p << std::endl;
+    std::cout << "Adresa uložená v pointeru p: " << p << std::endl;
 
-    // Uvolnění paměti
-    for(int i = 0; i < radky; i++){
-        delete[] matice[i];
-    }
-    delete[] matice;
+    *p = 20; // změna hodnoty a přes pointer
+
+    std::cout << "Nová hodnota a: " << a << std::endl;
 
     return 0;
 }
