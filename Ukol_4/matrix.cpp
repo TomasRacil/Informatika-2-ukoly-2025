df659e89ef272994af5d91c1d7af3b876d2a967e
Vojtech Jaeger <vojtajaeger@gmail.com>
2025-11-12 22:40:32 +0000

    Úkol 4 dokončeno

--- Ukol_4/matrix.cpp
+++ Ukol_4/matrix.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <stdexcept> // pro std::out_of_range
+using namespace std;
 
 // ===================================================================
 // KROK 1: IMPLEMENTACE Z ÚKOLU 3
@@ -24,72 +25,202 @@
 //
 // ... atd. pro (destruktor, getRows, getValue, setValue, subtract, transpose...)
 
+    void Matrix::allocateMatrix() 
+    {
+        if (rows_ <= 0 || cols_ <= 0) 
+        {
+            data_ = nullptr;
+            return;
+        }
+        data_ = new int*[rows_];
+        for (int i = 0; i < rows_; i++) 
+        {
+            data_[i] = new int[cols_];
+            for (int j = 0; j < cols_; j++) 
+            {
+                data_[i][j] = 0;
+            }
+        }
+    }
+    void Matrix::deallocateMatrix() 
+    {
+        if(data_!=nullptr)
+        {
+            for(int i=0;i<rows_;i++)
+            {
+                delete[] data_[i];
+            }
+            delete[]data_;
+        }
+    }
+
+    Matrix::Matrix(int rows, int cols) 
+    {
+        rows_ = (rows > 0) ? rows : 0;
+        cols_ = (cols > 0) ? cols : 0;
+        allocateMatrix();
+    }
+
+    Matrix::~Matrix() 
+    {
+        deallocateMatrix();
+    }
+
+    Matrix::Matrix(const Matrix& other) 
+    {
+        rows_ = other.rows_;
+        cols_ = other.cols_;
+        allocateMatrix();
+        if(data_!=nullptr && other.data_!=nullptr)
+        {
+            for(int i=0;i<rows_;i++)
+            {
+                for(int j=0;j<cols_;j++)
+                {
+                    data_[i][j]=other.data_[i][j];
+                }
+            }
+        }
+    }
+
+    int Matrix::getRows() const 
+    {
+        return rows_;
+    }
+
+    int Matrix::getCols() const 
+    {
+        return cols_; // Nahraďte
+    }
+
+    int Matrix::getValue(int row, int col) const 
+    {
+
+        if (row < 0 || row >= rows_ || col < 0 || col >= cols_) 
+        {
+            throw std::out_of_range("Mimo dosah");
+        }
+        if (data_ == nullptr) 
+        {
+            throw std::out_of_range("Mimo dosah");
+        }
+        return data_[row][col];
+    }
+
+    void Matrix::setValue(int row, int col, int value) 
+    {
+        if (row < 0 || row >= rows_ || col < 0 || col >= cols_) 
+        {
+            throw std::out_of_range("Mimo dosah");
+        }
+        if (data_ == nullptr) 
+        {
+            throw std::out_of_range("Mimo dosah");
+        }
+        data_[row][col] = value;
+    }
+
 
 // ===================================================================
 // KROK 2: IMPLEMENTACE OPERÁTORŮ PRO ÚKOL 4
 // ===================================================================
 
-Matrix::Matrix()
-{
-}
-
-Matrix::Matrix(int rows, int cols)
-{
-}
-
-Matrix::~Matrix()
-{
-}
-
-Matrix::Matrix(const Matrix &other)
-{
-}
-
-int Matrix::getRows() const
-{
-    return 0;
-}
-
-int Matrix::getCols() const
-{
-    return 0;
-}
-
-int Matrix::getValue(int row, int col) const
-{
-    return 0;
-}
-
-void Matrix::setValue(int row, int col, int value)
-{
-}
-
 Matrix Matrix::subtract(const Matrix &other)
 {
-    return Matrix();
+    if(this->rows_ != other.rows_ || cols_ != other.cols_) 
+    {
+        return Matrix(0, 0);
+    }      
+    Matrix result(rows_, cols_);
+    for(int i=0;i<rows_;i++)
+    {
+        for(int j=0;j<cols_;j++)
+        {
+            result.data_[i][j]=data_[i][j]-other.data_[i][j];
+        }
+    }
+    return result;
 }
 
 Matrix Matrix::T()
 {
-    return Matrix();
+    Matrix result(cols_,rows_);
+    for(int i=0;i<rows_;i++)
+    {
+        for(int j=0;j<cols_;j++)
+        {
+            result.data_[j][i]=data_[i][j];
+        }
+    }
+    return result;
 }
 
 Matrix Matrix::operator+(const Matrix &other)
 {
-    return Matrix();
+    if(this->rows_ != other.rows_ || cols_ != other.cols_) 
+    {
+        return Matrix(0, 0);
+    }      
+    Matrix result(rows_, cols_);
+    for(int i=0;i<rows_;i++)
+    {
+        for(int j=0;j<cols_;j++)
+        {
+            result.data_[i][j]=data_[i][j]+other.data_[i][j];
+        }
+    }
+    return result;
 }
 
 Matrix Matrix::operator*(const Matrix &other)
 {
-    return Matrix();
+    if(cols_!=other.rows_)
+    return Matrix(0,0);
+    Matrix result(rows_,other.cols_);
+    for(int i=0;i<rows_;i++)
+    {
+        for(int j=0;j<other.cols_;j++)
+        {
+            int summ=0;
+            for(int k=0;k<cols_;k++)
+            {
+                summ+=data_[i][k]*other.data_[k][j];
+            }
+            result.data_[i][j]=summ;
+        }
+    }
+    return result;
 }
 
 Matrix Matrix::operator*(int scalar)
-{
-    return Matrix();
+{     
+    Matrix result(rows_, cols_);
+    if(!data_) return result;
+    for(int i=0;i<rows_;i++)
+    {
+        for(int j=0;j<cols_;j++)
+        {
+            result.data_[i][j]=data_[i][j]*scalar;
+        }
+    }
+    return result;
 }
 
 std::ostream &operator<<(std::ostream &os, const Matrix &mat)
 {
+    os<<"Matice /"<<mat.rows_<<"x"<<mat.cols_<<")"<<endl;
+    if(!mat.data_)
+    {
+        os<<"prazdna matice"<<endl;
+        return os;
+    }
+    for(int i=0;i<mat.rows_;i++)
+    {
+        for(int j=0;j<mat.cols_;j++)
+        {
+            os<< setw(4)<<mat.data_[i][j];
+        }
+        os<<endl;
+    }
     return os;
 }
