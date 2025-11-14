249c1ba036eb47d642f826f1a5d9fc6f7174671d
S7L7 <buijohn@seznam.cz>
2025-11-14 22:13:58 +0100

    oprava cmake buidl

--- Ukol_3/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/Informatika-2-ukoly-2025-JohnBui/Ukol_3
-# Build directory: /workspaces/Informatika-2-ukoly-2025-JohnBui/Ukol_3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(mytests "/workspaces/Informatika-2-ukoly-2025-JohnBui/Ukol_3/build/mytests")
-set_tests_properties(mytests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/Informatika-2-ukoly-2025-JohnBui/Ukol_3/CMakeLists.txt;17;add_test;/workspaces/Informatika-2-ukoly-2025-JohnBui/Ukol_3/CMakeLists.txt;0;")
