0f949f613e826941635ef823df34669fce51994f
Lukas Kalensky <lukas.kalensky@unob.cz>
2025-12-01 19:02:26 +0000

    ukol5_pridani

---
+++ Ukol_5/valecnik.cpp
@@ -0,0 +1,27 @@
+#include "valecnik.h"
+
+Valecnik::Valecnik(const std::string& jmeno, double zivoty, double sila, double brneni)
+    : Postava(jmeno, zivoty, sila) { // Volání konstruktoru předka
+    // TODO: Inicializujte atribut _brneni
+}
+
+void Valecnik::utok(Postava& cil) {
+    // TODO: Implementujte útok válečníka
+    // 1. Zkontrolujte, zda má válečník méně než 30 % maximálních životů (berserk mode).
+    //    (Pozor: v zadání není max_hp, pro zjednodušení předpokládejme, že < 30 HP = berserk)
+    // 2. Pokud ano, vypište hlášku o zuřivosti a útočte silou * 1.5
+    // 3. Pokud ne, útočte normální silou.
+    // Tip: Využijte metodu cil.prijmiUtok()
+}
+
+void Valecnik::prijmiUtok(double poskozeni) {
+    // TODO: Implementujte obranu válečníka
+    // 1. Snižte příchozí poškození o hodnotu _brneni.
+    // 2. Pokud je výsledek menší než 0, nastavte ho na 0.
+    // 3. Vypište informaci o blokování.
+    // 4. Zavolejte metodu předka Postava::prijmiUtok() se sníženým poškozením.
+}
+
+void Valecnik::vypisInfo() const {
+    std::cout << "[Valecnik] " << _jmeno << " | HP: " << _zivoty << " | Armor: " << _brneni << std::endl;
+}
