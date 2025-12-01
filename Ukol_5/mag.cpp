7b6b8a6857f02eff06e4f8b049b7d246daf02c15
pajanekkk <phovjadsky@gmail.com>
2025-12-01 23:05:42 +0000

    pridani ukolu 5

--- Ukol_5/mag.cpp
+++ Ukol_5/mag.cpp
@@ -1,12 +1,15 @@
 #include "mag.h"
 
-Mag::Mag(const std::string& jmeno, double zivoty, double sila, double mana)
-    : Postava(jmeno, zivoty, sila) {
-    // TODO: Inicializujte atribut _mana
+using namespace std;
+
+Mag::Mag(const std::string &jmeno, double zivoty, double sila, double mana)
+    : Postava(jmeno, zivoty, sila)
+{
+    _mana = mana;
 }
 
-void Mag::utok(Postava& cil) {
-    // TODO: Implementujte maguv útok
+void Mag::utok(Postava &cil)
+{
     // 1. Pokud má mág dostatek many (>= 10):
     //    - Vypište "Sesila FIREBALL"
     //    - Způsobte poškození: _sila * 2
@@ -15,8 +18,24 @@ void Mag::utok(Postava& cil) {
     //    - Vypište "Dosla mana, uderi holi"
     //    - Způsobte poškození: _sila * 0.5
     //    - Přičtěte 5 many (regenerace)
+    double poskozeni;
+    if (_mana >= 10)
+    {
+        cout << "Sesila FIREBALL" << endl;
+        poskozeni = _sila * 2;
+        cil.prijmiUtok(poskozeni);
+        _mana -= 10;
+    }
+    else
+    {
+        cout << "Dosla mana, uderi holi" << endl;
+        poskozeni = _sila * 0.5;
+        cil.prijmiUtok(poskozeni);
+        _mana += 5;
+    }
 }
 
-void Mag::vypisInfo() const {
+void Mag::vypisInfo() const
+{
     std::cout << "[Mag] " << _jmeno << " | HP: " << _zivoty << " | Mana: " << _mana << std::endl;
 }
--- Ukol_5/valecnik.cpp
+++ Ukol_5/valecnik.cpp
@@ -1,27 +1,51 @@
 #include "valecnik.h"
 
-Valecnik::Valecnik(const std::string& jmeno, double zivoty, double sila, double brneni)
-    : Postava(jmeno, zivoty, sila) { // Volání konstruktoru předka
-    // TODO: Inicializujte atribut _brneni
+using namespace std;
+
+Valecnik::Valecnik(const std::string &jmeno, double zivoty, double sila, double brneni)
+    : Postava(jmeno, zivoty, sila)
+{ // Volání konstruktoru předka
+    _brneni = brneni;
 }
 
-void Valecnik::utok(Postava& cil) {
-    // TODO: Implementujte útok válečníka
+void Valecnik::utok(Postava &cil)
+{
+    // Implementujte útok válečníka
     // 1. Zkontrolujte, zda má válečník méně než 30 % maximálních životů (berserk mode).
     //    (Pozor: v zadání není max_hp, pro zjednodušení předpokládejme, že < 30 HP = berserk)
     // 2. Pokud ano, vypište hlášku o zuřivosti a útočte silou * 1.5
     // 3. Pokud ne, útočte normální silou.
     // Tip: Využijte metodu cil.prijmiUtok()
+    double b_poskozeni = _sila * 1.5; // b jako berserk
+
+    if (_zivoty < (0.3 * _maxZivoty))
+    {
+        cout << "Zurivost" << endl;
+        cil.prijmiUtok(b_poskozeni);
+    }
+    else
+    {
+        cil.prijmiUtok(_sila);
+    }
 }
 
-void Valecnik::prijmiUtok(double poskozeni) {
-    // TODO: Implementujte obranu válečníka
+void Valecnik::prijmiUtok(double poskozeni)
+{
+    // Implementujte obranu válečníka
     // 1. Snižte příchozí poškození o hodnotu _brneni.
     // 2. Pokud je výsledek menší než 0, nastavte ho na 0.
     // 3. Vypište informaci o blokování.
     // 4. Zavolejte metodu předka Postava::prijmiUtok() se sníženým poškozením.
+    double blokovane = poskozeni - _brneni;
+    if (blokovane < 0)
+    {
+        blokovane = 0;
+    }
+    cout << "Utok blokovan" << endl;
+    Postava::prijmiUtok(blokovane);
 }
 
-void Valecnik::vypisInfo() const {
+void Valecnik::vypisInfo() const
+{
     std::cout << "[Valecnik] " << _jmeno << " | HP: " << _zivoty << " | Armor: " << _brneni << std::endl;
 }
