a4abb66becc586d18f695e0663e6710602b545b2
Lupna123 <lukas.pnacek@seznam.cz>
2025-12-02 17:14:29 +0100

    ukol_5

---
+++ Ukol_5/mag.cpp
@@ -0,0 +1,27 @@
+#include "mag.h"
+
+Mag::Mag(const std::string& jmeno, double zivoty, double sila, double mana)
+    : Postava(jmeno, zivoty, sila) {
+        _mana = mana;
+}
+
+void Mag::utok(Postava& cil) {
+    if(jeZiva())
+    {
+        if(_mana >= 10){
+            std::cout << this->getJmeno() << "Sesila FIREBALL" << std::endl;
+            cil.prijmiUtok(this->_sila * 2);
+            _mana -= 10;
+        }
+        else
+        {
+            std::cout << this->getJmeno() << "Dosla mana, uderi holi" << std::endl;
+            cil.prijmiUtok(this-> _sila * 0.5);
+            _mana += 5;
+        }
+    }
+}
+
+void Mag::vypisInfo() const {
+    std::cout << "[Mag] " << _jmeno << " | HP: " << _zivoty << " | Mana: " << _mana << std::endl;
+}
---
+++ Ukol_5/valecnik.cpp
@@ -0,0 +1,40 @@
+#include "valecnik.h"
+
+Valecnik::Valecnik(const std::string& jmeno, double zivoty, double sila, double brneni)
+    : Postava(jmeno, zivoty, sila) { // Volání konstruktoru předka
+
+    _brneni = brneni;
+}
+
+void Valecnik::utok(Postava& cil) {
+
+    if(jeZiva())
+    {
+        if(_zivoty < 0,3 * _maxZivoty)
+        {
+            std::cout << this->getJmeno() << "je v zurivem modu a utoci silou: " << this->_sila * 1.5 << std::endl;
+            cil.prijmiUtok(this->_sila * 1.5);
+        }
+        else
+        {
+            std::cout << this->getJmeno() << "utoci na " << cil.getJmeno() << "silou " << this->_sila << std::endl;
+            cil.prijmiUtok(this->_sila);
+        }
+    }
+}
+
+void Valecnik::prijmiUtok(double poskozeni) {
+
+    double snizenePoskozeni = poskozeni - _brneni;
+
+    if(snizenePoskozeni < 0)
+    {
+        snizenePoskozeni = 0;
+    }
+    std::cout << this->getJmeno() << "blokuje " << _brneni << "poskozeni brnenim!" << std::endl;
+    Postava::prijmiUtok(snizenePoskozeni);
+}
+
+void Valecnik::vypisInfo() const {
+    std::cout << "[Valecnik] " << _jmeno << " | HP: " << _zivoty << " | Armor: " << _brneni << std::endl;
+}
