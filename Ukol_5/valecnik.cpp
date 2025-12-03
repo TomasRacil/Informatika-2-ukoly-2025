518c820b7a9d47c8a2c5f5b75adb58244f860cef
Vojtech Jaeger <vojtajaeger@gmail.com>
2025-12-03 12:44:07 +0000

    Úkol 5 dokončeno

--- Ukol_5/valecnik.cpp
+++ Ukol_5/valecnik.cpp
@@ -1,7 +1,7 @@
 #include "valecnik.h"
 
 Valecnik::Valecnik(const std::string& jmeno, double zivoty, double sila, double brneni)
-    : Postava(jmeno, zivoty, sila) { // Volání konstruktoru předka
+    : Postava(jmeno, zivoty, sila), _brneni(brneni) { // Volání konstruktoru předka
     // TODO: Inicializujte atribut _brneni
 }
 
@@ -12,6 +12,17 @@ void Valecnik::utok(Postava& cil) {
     // 2. Pokud ano, vypište hlášku o zuřivosti a útočte silou * 1.5
     // 3. Pokud ne, útočte normální silou.
     // Tip: Využijte metodu cil.prijmiUtok()
+    double utocnaSila=_sila;
+    if(_zivoty < 0.3 * _maxZivoty)
+    {
+        std::cout << _jmeno << " je v zurivem modu a utoci vetsi silou!" << std::endl;
+        utocnaSila *=1.5;
+    }
+        
+    std::cout << _jmeno << "utoci na " << cil.getJmeno() << "se silou " << utocnaSila << std::endl;
+    cil.prijmiUtok(utocnaSila);
+
+
 }
 
 void Valecnik::prijmiUtok(double poskozeni) {
@@ -20,6 +31,13 @@ void Valecnik::prijmiUtok(double poskozeni) {
     // 2. Pokud je výsledek menší než 0, nastavte ho na 0.
     // 3. Vypište informaci o blokování.
     // 4. Zavolejte metodu předka Postava::prijmiUtok() se sníženým poškozením.
+    std::cout << _jmeno << " blokuje " << _brneni << "poskozeni" << std::endl;
+    double snizenePoskozeni = poskozeni - _brneni;
+    if(snizenePoskozeni<0)
+    {
+        snizenePoskozeni=0;
+    }
+    Postava::prijmiUtok(snizenePoskozeni);
 }
 
 void Valecnik::vypisInfo() const {
