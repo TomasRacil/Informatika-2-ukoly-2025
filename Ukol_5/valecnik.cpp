a53f329075d67e1addd0a0c34ea6b83bc0960133
MalekMatej <matej.tre@gmail.com>
2025-12-03 13:46:57 +0100

    Update valecnik.cpp

--- Ukol_5/valecnik.cpp
+++ Ukol_5/valecnik.cpp
@@ -1,25 +1,33 @@
 #include "valecnik.h"
 
 Valecnik::Valecnik(const std::string& jmeno, double zivoty, double sila, double brneni)
-    : Postava(jmeno, zivoty, sila) { // Volání konstruktoru předka
-    // TODO: Inicializujte atribut _brneni
+    : Postava(jmeno, zivoty, sila), _brneni(brneni) { 
 }
 
 void Valecnik::utok(Postava& cil) {
-    // TODO: Implementujte útok válečníka
-    // 1. Zkontrolujte, zda má válečník méně než 30 % maximálních životů (berserk mode).
-    //    (Pozor: v zadání není max_hp, pro zjednodušení předpokládejme, že < 30 HP = berserk)
-    // 2. Pokud ano, vypište hlášku o zuřivosti a útočte silou * 1.5
-    // 3. Pokud ne, útočte normální silou.
-    // Tip: Využijte metodu cil.prijmiUtok()
+    double dmg = _sila;
+
+    if (_zivoty < 30) {
+        std::cout << _jmeno << " upada do ZURENÍ (BERSERK)!" << std::endl;
+        dmg *= 1.5;
+    }
+
+    std::cout << _jmeno << " utoci mecem silou " << dmg << std::endl;
+
+    cil.prijmiUtok(dmg);
 }
 
 void Valecnik::prijmiUtok(double poskozeni) {
-    // TODO: Implementujte obranu válečníka
-    // 1. Snižte příchozí poškození o hodnotu _brneni.
-    // 2. Pokud je výsledek menší než 0, nastavte ho na 0.
-    // 3. Vypište informaci o blokování.
-    // 4. Zavolejte metodu předka Postava::prijmiUtok() se sníženým poškozením.
+   double realPoskozeni = poskozeni - _brneni;
+
+    if (realPoskozeni < 0)
+        realPoskozeni = 0;
+
+    std::cout << _jmeno << " blokuje " << _brneni
+              << " poskozeni brnenim. Utrpene poskozeni: "
+              << realPoskozeni << std::endl;
+
+    Postava::prijmiUtok(realPoskozeni);
 }
 
 void Valecnik::vypisInfo() const {
