dc229ce32ca5fce0eaa0d4ee875b7768caa55ef5
MalekMatej <matej.tre@gmail.com>
2025-12-03 13:45:21 +0100

    Update mag.cpp

--- Ukol_5/mag.cpp
+++ Ukol_5/mag.cpp
@@ -1,20 +1,26 @@
 #include "mag.h"
 
 Mag::Mag(const std::string& jmeno, double zivoty, double sila, double mana)
-    : Postava(jmeno, zivoty, sila) {
-    // TODO: Inicializujte atribut _mana
+    : Postava(jmeno, zivoty, sila), _mana(mana) {
 }
 
 void Mag::utok(Postava& cil) {
-    // TODO: Implementujte maguv útok
-    // 1. Pokud má mág dostatek many (>= 10):
-    //    - Vypište "Sesila FIREBALL"
-    //    - Způsobte poškození: _sila * 2
-    //    - Odečtěte 10 many
-    // 2. Pokud nemá dostatek many:
-    //    - Vypište "Dosla mana, uderi holi"
-    //    - Způsobte poškození: _sila * 0.5
-    //    - Přičtěte 5 many (regenerace)
+    if (_mana >= 10) {
+        std::cout << _jmeno << " sesila FIREBALL!" << std::endl;
+
+        double dmg = _sila * 2;
+        cil.prijmiUtok(dmg);
+
+        _mana -= 10;
+    }
+    else {
+        std::cout << _jmeno << " dosla mana, uderi holi!" << std::endl;
+
+        double dmg = _sila * 0.5;
+        cil.prijmiUtok(dmg);
+
+        _mana += 5; 
+    }
 }
 
 void Mag::vypisInfo() const {
