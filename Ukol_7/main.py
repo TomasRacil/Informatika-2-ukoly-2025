2048ddaff3f5504efa53ba502450358d08d794e3
velkayolanda <tomo.simurda@gmail.com>
2025-12-12 15:57:41 +0000

    Ukol 7

--- Ukol_7/main.py
+++ Ukol_7/main.py
@@ -12,25 +12,21 @@ class Vector2D:
         """
         Vrací textovou reprezentaci vektoru ve formátu: Vector(x, y)
         """
-        # TODO: Implementujte formátovaný výpis
-        return ""
+        return f"Vector({self.x}, {self.y})"
 
     def __add__(self, other):
         """
         Operátor +: Sečte dva vektory.
         Vrací novou instanci Vector2D.
         """
-        # TODO: Implementujte sčítání
-        # return Vector2D(..., ...)
-        return None
+        return Vector2D(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
         """
         Operátor -: Odečte druhý vektor od prvního.
         Vrací novou instanci Vector2D.
         """
-        # TODO: Implementujte odčítání
-        return None
+        return Vector2D(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other):
         """
@@ -39,11 +35,9 @@ class Vector2D:
         - Pokud je 'other' Vector2D, provede skalární součin a vrátí číslo.
         """
         if isinstance(other, (int, float)):
-            # TODO: Implementujte násobení skalárem (číslem)
-            return None
+            return Vector2D(self.x * other, self.y * other)
         elif isinstance(other, Vector2D):
-            # TODO: Implementujte skalární součin (vektor * vektor)
-            return 0
+            return self.x * other.x + self.y * other.y
         else:
             raise TypeError("Násobení je podporováno pouze s číslem nebo jiným vektorem.")
 
@@ -51,15 +45,13 @@ class Vector2D:
         """
         Operátor ==: Porovná dva vektory na shodu.
         """
-        # TODO: Implementujte porovnání
-        return False
+        return self.x == other.x and self.y == other.y
 
     def __abs__(self):
         """
         Funkce abs(): Vrátí délku (magnitudu) vektoru.
         """
-        # TODO: Implementujte výpočet délky (Pythagorova věta)
-        return 0.0
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
 if __name__ == "__main__":
     # Zde si můžete zkoušet svůj kód ručně
