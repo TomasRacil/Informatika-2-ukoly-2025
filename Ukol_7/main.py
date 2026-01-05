e53ee71e3d737b22997bf27157d30eb94167c078
JanJasansky <jan.jasansky@unob.cz>
2026-01-05 16:12:20 +0000

    Ukol 7

--- Ukol_7/main.py
+++ Ukol_7/main.py
@@ -12,25 +12,25 @@ class Vector2D:
         """
         Vrací textovou reprezentaci vektoru ve formátu: Vector(x, y)
         """
-        # TODO: Implementujte formátovaný výpis
-        return ""
+        return f"Vector({self.x}, {self.y})"
 
     def __add__(self, other):
         """
         Operátor +: Sečte dva vektory.
         Vrací novou instanci Vector2D.
         """
-        # TODO: Implementujte sčítání
-        # return Vector2D(..., ...)
-        return None
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x + other.x, self.y + other.y)
+        raise TypeError("Sčítání je podporováno pouze mezi dvěma vektory.")
 
     def __sub__(self, other):
         """
         Operátor -: Odečte druhý vektor od prvního.
         Vrací novou instanci Vector2D.
         """
-        # TODO: Implementujte odčítání
-        return None
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x - other.x, self.y - other.y)
+        raise TypeError("Odčítání je podporováno pouze mezi dvěma vektory.")
 
     def __mul__(self, other):
         """
@@ -39,11 +39,9 @@ class Vector2D:
         - Pokud je 'other' Vector2D, provede skalární součin a vrátí číslo.
         """
         if isinstance(other, (int, float)):
-            # TODO: Implementujte násobení skalárem (číslem)
-            return None
+            return Vector2D(self.x * other, self.y * other)
         elif isinstance(other, Vector2D):
-            # TODO: Implementujte skalární součin (vektor * vektor)
-            return 0
+            return self.x * other.x + self.y * other.y
         else:
             raise TypeError("Násobení je podporováno pouze s číslem nebo jiným vektorem.")
 
@@ -51,18 +49,24 @@ class Vector2D:
         """
         Operátor ==: Porovná dva vektory na shodu.
         """
-        # TODO: Implementujte porovnání
+        if isinstance(other, Vector2D):
+            return self.x == other.x and self.y == other.y
         return False
 
     def __abs__(self):
         """
         Funkce abs(): Vrátí délku (magnitudu) vektoru.
         """
-        # TODO: Implementujte výpočet délky (Pythagorova věta)
-        return 0.0
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
 if __name__ == "__main__":
     # Zde si můžete zkoušet svůj kód ručně
     v1 = Vector2D(3, 4)
+    v2 = Vector2D(1, 2)
     print(f"Můj vektor: {v1}")
     print(f"Délka: {abs(v1)}")
+    print(f"Sčítání: {v1 + v2}")
+    print(f"Odčítání: {v1 - v2}")
+    print(f"Skalární násobení číslem: {v1 * 2}")
+    print(f"Skalární součin: {v1 * v2}")
+    print(f"Porovnání: {v1 == v2}")
