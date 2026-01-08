42bb89cf049808430e7b9f87916834c8a08dc44f
Svejkys <x5pm2q29r5@privaterelay.appleid.com>
2026-01-08 14:21:13 +0100

    Revert "ukol7"
    This reverts commit 3bff37a025aeadd38d682e5a1205ed6a5e96ee85.

---
+++ Ukol_5/mag.cpp
@@ -0,0 +1,41 @@
+#include "mag.h"
+
+Mag::Mag(const std::string& jmeno, double zivoty, double sila, double mana)
+    : Postava(jmeno, zivoty, sila) {
+
+    _mana = mana;
+}
+
+void Mag::utok(Postava& cil) {
+    if (jeZiva()) {
+        if (_mana >= 10) {
+            std::cout << this->getJmeno() << " sesila FIREBALL!" << std::endl;
+            cil.prijmiUtok(this->_sila * 2);
+            _mana -= 10;
+        } else {
+            std::cout << this->getJmeno() << " dosla mana, uderi holi!" << std::endl;
+            cil.prijmiUtok(this->_sila * 0.5);
+            _mana += 5;
+        }
+    }
+
+
+    // TODO: Inicializujte atribut _mana
+}
+
+void Mag::utok(Postava& cil) {
+    // TODO: Implementujte maguv útok
+    // 1. Pokud má mág dostatek many (>= 10):
+    //    - Vypište "Sesila FIREBALL"
+    //    - Způsobte poškození: _sila * 2
+    //    - Odečtěte 10 many
+    // 2. Pokud nemá dostatek many:
+    //    - Vypište "Dosla mana, uderi holi"
+    //    - Způsobte poškození: _sila * 0.5
+    //    - Přičtěte 5 many (regenerace)
+
+}
+
+void Mag::vypisInfo() const {
+    std::cout << "[Mag] " << _jmeno << " | HP: " << _zivoty << " | Mana: " << _mana << std::endl;
+}
