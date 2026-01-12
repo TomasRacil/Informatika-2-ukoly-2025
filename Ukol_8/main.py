9ecb8d481176cab67dd242e9cb494774826c0d71
Zuzana Kršková <zuzan.krskova2@unob.cz>
2026-01-12 20:50:52 +0100

    Odevzdani Ukol 8 - Zuzana Krskova

--- Ukol_8/main.py
+++ Ukol_8/main.py
@@ -7,9 +7,12 @@ def read_logs(file_path):
     """
     try:
         # TODO: Otevřít soubor a yieldovat řádky
-        pass
+        with open(file_path, 'r') as file:
+            for line in file:
+                yield line.strip()
     except FileNotFoundError:
         print(f"Chyba: Soubor '{file_path}' nebyl nalezen.")
+    pass
 
 def process_line(line):
     """
@@ -21,11 +24,17 @@ def process_line(line):
     """
     # TODO: Definovat regex pattern
     # pattern = r"..."
-    
+    pattern = r"\[(?P<timestamp>.*?)\] (?P<level>\w+): (?P<message>.*?) - User: (?P<email>[\w\.\+\-]+@[\w\.\-]+)"
+
     # TODO: Použít re.search nebo re.match
     # match = ...
+    match = re.match(pattern, line)
     
     # TODO: Pokud match, vrátit dict, jinak None
+    if match:
+        return match.groupdict()
+    else:
+        return None
     pass
 
 def analyze_logs(input_file, output_file):
@@ -39,8 +48,15 @@ def analyze_logs(input_file, output_file):
         # TODO: Zpracovat řádek přes process_line
         # TODO: Pokud je level == 'ERROR', zapsat do souboru
         # pass
-    
+    with open(output_file, 'w') as f_out:
+        for line in read_logs(input_file):
+            processed = process_line(line)
+            if processed and processed['level'] == 'ERROR':
+                f_out.write(f"{processed['timestamp']} - {processed['message']} - {processed['email']}\n")
+                count += 1
+
     print(f"Zpracování dokončeno. Nalezeno {count} chyb.")
+    pass
 
 if __name__ == "__main__":
     # Pro účely testování vytvořte soubor data.log, pokud neexistuje, nebo jej stáhněte
