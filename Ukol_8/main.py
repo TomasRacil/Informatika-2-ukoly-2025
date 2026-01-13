6f91664caeef80ae23dd329f7faee1771b288d13
pajanekkk <phovjadsky@gmail.com>
2026-01-13 10:02:12 +0000

    pridani ukolu 8

--- Ukol_8/main.py
+++ Ukol_8/main.py
@@ -1,47 +1,53 @@
 import re
 
+
 def read_logs(file_path):
     """
     Generátor, který načítá soubor řádek po řádku.
     Ošetřete FileNotFoundError.
     """
     try:
-        # TODO: Otevřít soubor a yieldovat řádky
-        pass
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                yield line
     except FileNotFoundError:
         print(f"Chyba: Soubor '{file_path}' nebyl nalezen.")
+
 
 def process_line(line):
     """
     Zpracuje řádek logu pomocí regexu.
     Vrátí slovník {'timestamp': ..., 'level': ..., 'message': ..., 'email': ...}
     nebo None, pokud řádek neodpovídá formátu.
-    
+
     Očekávaný formát: [DATUM] LEVEL: Zpráva - User: email
     """
-    # TODO: Definovat regex pattern
-    # pattern = r"..."
-    
-    # TODO: Použít re.search nebo re.match
-    # match = ...
-    
-    # TODO: Pokud match, vrátit dict, jinak None
-    pass
+    pattern = r"\[(?P<timestamp>.*?)\] (?P<level>\w+): (?P<message>.*?) - User: (?P<email>[\w\. \+-]+@[\w\.-]+)"
+
+    match = re.search(pattern, line)
+
+    if match:
+        return match.groupdict()
+    return None
+
 
 def analyze_logs(input_file, output_file):
     """
     Načte logy, vyfiltruje ERROR záznamy a zapíše je do výstupního souboru.
     """
     count = 0
-    # TODO: Otevřít output_file pro zápis
-    # with open(output_file, 'w') as f_out:
-        # TODO: Iterovat přes read_logs(input_file)
-        # TODO: Zpracovat řádek přes process_line
-        # TODO: Pokud je level == 'ERROR', zapsat do souboru
-        # pass
-    
+
+    with open(output_file, 'w', encoding='utf-8') as f_out:
+        for line in read_logs(input_file):
+            data = process_line(line)
+            if data and data['level'] == 'ERROR':
+                f_out.write(
+                    f"[{data['timestamp']}] ERROR: {data['message']} - User: {data['email']}\n")
+                count += 1
+
     print(f"Zpracování dokončeno. Nalezeno {count} chyb.")
+
 
 if __name__ == "__main__":
     # Pro účely testování vytvořte soubor data.log, pokud neexistuje, nebo jej stáhněte
-    analyze_logs("sample_data.txt", "filtered_errors.txt")
+    analyze_logs("Ukol_8/sample_data.txt", "Ukol_8/filtered_errors.txt")
