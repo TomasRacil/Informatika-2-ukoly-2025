c1d0010589f53d25ef324c7c2273416a9be6e82d
Vojtech Jaeger <vojtajaeger@gmail.com>
2026-01-14 13:10:37 +0000

    Úkol 8 dokončeno

--- Ukol_8/main.py
+++ Ukol_8/main.py
@@ -6,8 +6,9 @@ def read_logs(file_path):
     Ošetřete FileNotFoundError.
     """
     try:
-        # TODO: Otevřít soubor a yieldovat řádky
-        pass
+        with open(file_path,"r",encoding="utf-8") as f:
+            for line in f:
+                yield line.strip()
     except FileNotFoundError:
         print(f"Chyba: Soubor '{file_path}' nebyl nalezen.")
 
@@ -19,27 +20,28 @@ def process_line(line):
     
     Očekávaný formát: [DATUM] LEVEL: Zpráva - User: email
     """
-    # TODO: Definovat regex pattern
-    # pattern = r"..."
+    regex_pattern = r"\[(?P<timestamp>.*?)\] (?P<level>\w+): (?P<message>.*?) - User: (?P<email>.*)"
     
-    # TODO: Použít re.search nebo re.match
-    # match = ...
-    
-    # TODO: Pokud match, vrátit dict, jinak None
-    pass
+    match=re.match(regex_pattern,line)
+    if match:
+        return match.groupdict()
+    else:
+        return None
+
 
 def analyze_logs(input_file, output_file):
     """
     Načte logy, vyfiltruje ERROR záznamy a zapíše je do výstupního souboru.
     """
-    count = 0
-    # TODO: Otevřít output_file pro zápis
-    # with open(output_file, 'w') as f_out:
-        # TODO: Iterovat přes read_logs(input_file)
-        # TODO: Zpracovat řádek přes process_line
-        # TODO: Pokud je level == 'ERROR', zapsat do souboru
-        # pass
-    
+    count=0
+
+    with open(output_file, 'w') as f_out:
+            for line in read_logs(input_file):
+                processed = process_line(line)
+                if processed and processed['level'] == 'ERROR':
+                    f_out.write(f"{processed['timestamp']} - {processed['message']} - {processed['email']}\n")
+                    count += 1
+
     print(f"Zpracování dokončeno. Nalezeno {count} chyb.")
 
 if __name__ == "__main__":
