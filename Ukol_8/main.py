e2f6fb32c2c779bbd82344fb3942ce5be8344ed3
Matyas Kopecek <matykopecek@gmail.com>
2026-01-14 15:55:26 +0000

    Vypracovani ukolu 8

--- Ukol_8/main.py
+++ Ukol_8/main.py
@@ -1,47 +1,39 @@
 import re
 
 def read_logs(file_path):
-    """
-    Generátor, který načítá soubor řádek po řádku.
-    Ošetřete FileNotFoundError.
-    """
     try:
-        # TODO: Otevřít soubor a yieldovat řádky
-        pass
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                yield line.strip()
+        
     except FileNotFoundError:
-        print(f"Chyba: Soubor '{file_path}' nebyl nalezen.")
+        print(f"Chyba Soubor {file_path} nebyl nalezen")
 
 def process_line(line):
-    """
-    Zpracuje řádek logu pomocí regexu.
-    Vrátí slovník {'timestamp': ..., 'level': ..., 'message': ..., 'email': ...}
-    nebo None, pokud řádek neodpovídá formátu.
     
-    Očekávaný formát: [DATUM] LEVEL: Zpráva - User: email
-    """
-    # TODO: Definovat regex pattern
-    # pattern = r"..."
+    pattern = r"\[(?P<timestamp>.*?)\]\s(?P<level>\w+):\s(?P<message>.*?)\s-\sUser:\s(?P<email>[\w\.\-\+]+@[\w\.-]+)"
     
-    # TODO: Použít re.search nebo re.match
-    # match = ...
+    match = re.match(pattern, line)
+    if match:
+        return match.groupdict()
     
-    # TODO: Pokud match, vrátit dict, jinak None
-    pass
+    return None
 
 def analyze_logs(input_file, output_file):
-    """
-    Načte logy, vyfiltruje ERROR záznamy a zapíše je do výstupního souboru.
-    """
     count = 0
-    # TODO: Otevřít output_file pro zápis
-    # with open(output_file, 'w') as f_out:
-        # TODO: Iterovat přes read_logs(input_file)
-        # TODO: Zpracovat řádek přes process_line
-        # TODO: Pokud je level == 'ERROR', zapsat do souboru
-        # pass
-    
-    print(f"Zpracování dokončeno. Nalezeno {count} chyb.")
+    try:
+        with open(output_file, 'w', encoding='utf-8') as f_out:
+            for line in read_logs(input_file):
+                processed = process_line(line)
+                
+                if processed and processed['level'] == 'ERROR':
+                    f_out.write(f"{processed['timestamp']} - {processed['message']} - {processed['email']}\n")
+                    count += 1    
+        
+        print(f"Zpracovani dokonceno Nalezeno {count} chyb")
+        
+    except Exception as e:
+        print(f"Nastala neocekavana chyba pri zapisu {e}")
 
 if __name__ == "__main__":
-    # Pro účely testování vytvořte soubor data.log, pokud neexistuje, nebo jej stáhněte
     analyze_logs("sample_data.txt", "filtered_errors.txt")
