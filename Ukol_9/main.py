8fedfd23eec63081ca0fccf4bbf0e8becc6ba4fd
USER <kaplaneklukas@gmail.com>
2026-01-16 16:34:57 +0000

    Ukol 9 done

--- Ukol_9/main.py
+++ Ukol_9/main.py
@@ -1,12 +1,14 @@
 import argparse
-import sys
+import datetime
 from models import Product
 from storage import Storage
 
 # TODO: Implementovat dekorátor @log_action (zapsat do history.log)
 def log_action(func):
     def wrapper(*args, **kwargs):
-        # ... logika logování ...
+        now = datetime.datetime.now()
+        with open("history.log", "a") as log_file:
+            log_file.write(f"{now} - {func.__name__} {args[1:]}\n")
         return func(*args, **kwargs)
     return wrapper
 
@@ -17,20 +19,39 @@ class InventoryManager:
 
     @log_action
     def add_product(self, name: str, price: float, quantity: int):
-        # TODO: Vytvořit produkt, přidat do self.products, uložit
+        produkt = Product(name, price, quantity)
+        self.products.append(produkt)
         print(f"Produkt {name} přidán.")
+        self.storage.save_products(self.products)
+
+    @log_action
+    def remove_product(self, name: str):
+        product = self.search_products(name)
+        if product:
+            self.products.remove(product)
+            print(f"Produkt {name} odstraněn.")
+            self.storage.save_products(self.products)
+        else:
+            print(f"Produkt {name} nenalezen.")
+
 
     def list_products(self):
-        # TODO: Vypsat všechny produkty
-        pass
+        for product in self.products:
+            print(f"Produkt {product._name} cena: {product._price} množství: {product._quantity}")
+
 
     def search_products(self, query: str):
-        # TODO: Vyhledat produkty obsahující query v názvu
-        pass
+        for product in self.products:
+            if query == product._name:
+                return product
+        return None
+
     
     def total_value(self):
-        # TODO: Spočítat celkovou hodnotu
-        pass
+        celkova_hodnota = 0
+        for product in self.products:
+            celkova_hodnota += product._price * product._quantity
+        print(f"Celková hodnota skladu: {celkova_hodnota}")
 
 def main():
     parser = argparse.ArgumentParser(description="Systém správy skladu")
@@ -42,12 +63,17 @@ def main():
     add_parser.add_argument("--price", required=True, type=float, help="Cena")
     add_parser.add_argument("--qty", required=True, type=int, help="Množství")
 
+    # Příkaz 'remove'
+    remove_parser = subparsers.add_parser("remove", help="Odstranit produkt")
+    remove_parser.add_argument("--name", required=True, help="Název produktu k odstranění")
+
     # Příkaz 'list'
     subparsers.add_parser("list", help="Vypsat produkty")
     
     # Příkaz 'search'
     search_parser = subparsers.add_parser("search", help="Hledat produkt")
     search_parser.add_argument("--query", required=True, help="Hledaný text")
+
 
     args = parser.parse_args()
     
@@ -56,11 +82,14 @@ def main():
 
     if args.command == "add":
         manager.add_product(args.name, args.price, args.qty)
+    elif args.command == "remove":
+        manager.remove_product(args.name)
     elif args.command == "list":
         manager.list_products()
     elif args.command == "search":
         manager.search_products(args.query)
-    # TODO: Další příkazy
+    elif args.command == "total":
+        manager.total_value()
     else:
         parser.print_help()
 
--- Ukol_9/models.py
+++ Ukol_9/models.py
@@ -4,17 +4,28 @@ class Product:
     """
     def __init__(self, name: str, price: float, quantity: int):
         # TODO: Inicializace, využití properties pro validaci
-        self._name = name
+        self.name = name
         self.price = price
         self.quantity = quantity
 
     @property
     def price(self) -> float:
         return self._price
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, value: str):
+        if not value:
+            raise ValueError("Název produktu nesmí být prázdný.")
+        self._name = value
 
     @price.setter
     def price(self, value: float):
-        # TODO: Validace, raise ValueError pokud < 0
+        if value < 0:
+            raise ValueError("Cena nemůže být záporná.")
         self._price = value
 
     @property
@@ -23,7 +34,8 @@ class Product:
 
     @quantity.setter
     def quantity(self, value: int):
-        # TODO: Validace
+        if value < 0:
+            raise ValueError("Množství nemůže být záporné.")
         self._quantity = value
 
     def to_dict(self) -> dict:
@@ -40,5 +52,4 @@ class Product:
         return Product(data['name'], data['price'], data['quantity'])
 
     def __str__(self) -> str:
-        # TODO: Hezký výpis
-        return f"{self._name}"
+        return f"Produkt {self._name} cena: {self._price} množství: {self._quantity}"
